92bd8120e06cc145bc5e8b966e4488c2da2bccec
jagadishtagon <jagadish6060@gmail.com>
2019-05-22 21:34:10 +0530

    chnages in pagination

--- core/views.py
+++ core/views.py
@@ -7,18 +7,38 @@ from django.shortcuts import render
 # paginator = Paginator(user_list, 10)
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+import json
+from .serializers import *
+from django.http import HttpResponse, JsonResponse
+from .pagination import *
+from django.views.decorators.csrf import csrf_exempt
+# def index(request):
+#     user_list = User.objects.all()
+#     page = request.GET.get('page', 1)
+#
+#     paginator = Paginator(user_list, 5)
+#     print (paginator)
+#     try:
+#         users = paginator.page(page)
+#     except PageNotAnInteger:
+#         users = paginator.page(1)
+#     except EmptyPage:
+#         users = paginator.page(paginator.num_pages)
+#
+#     return render(request, 'user_list.html', { 'users': users })
+@csrf_exempt
 def index(request):
+    #data = json.loads(request.body.decode('utf-8'))
+    #offset, limit = get_offset(data)
     user_list = User.objects.all()
     page = request.GET.get('page', 1)
 
-    paginator = Paginator(user_list, 5)
-    print (paginator)
+    paginator = Paginator(user_list, 2)
     try:
         users = paginator.page(page)
     except PageNotAnInteger:
         users = paginator.page(1)
     except EmptyPage:
         users = paginator.page(paginator.num_pages)
-
-    return render(request, 'user_list.html', { 'users': users })
+    user = UserSerilaizer(users,many=True)
+    return JsonResponse({"response": user.data, "status": "success"})
